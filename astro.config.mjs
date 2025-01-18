397f35a43d071d79ca5c3985b74b2c78a6c4e450
ronaldjames6548 <ronaldjames6548@gmail.com>
2025-01-18 20:38:33 +0500

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astros.zank.studio",
+	site: "https://stiktokio.com",
 	vite: {
 		define: {
 			__DATE__: `'${new Date().toISOString()}'`,
